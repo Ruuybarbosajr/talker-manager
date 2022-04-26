1777a95465c37d0914faf8bfcae1240dbfd00abb
Ruuybarbosajr <ruuybarbosajr@icloud.com>
2022-04-26 00:15:25 -0300

    ref

--- middlewares/getTalkersByQuery.js
+++ middlewares/getTalkersByQuery.js
@@ -4,7 +4,7 @@ async function getTalkersByQuery(req, res, next) {
   try {
     const { q } = req.query;
     const readFile = await fileSystem.readFile('talker.json');
-    const dataFilteredByQuery = readFile.filter(({ name }) => name.includes(q));
+    const dataFilteredByQuery = readFile.filter(({ name }) => name.includes(q || ''));
     return res.status(200).json(dataFilteredByQuery);
   } catch (error) {
     next(error);
